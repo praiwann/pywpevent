8fa3fcfc65e2b53b65c50f05463e2b21ec4d2544
Unknown <npraiwan@outlook.com>
2018-07-29 16:08:14 +0700

    Initialize project for test

---
+++ ctrl/__init__.py
@@ -0,0 +1,1 @@
+from ctrl.main import initialize, add_action, add_filter, do_action, apply_filter
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pywpevent',
+    packages=['pywpevent'],
+    version='1.0',
+    description='Simple event management like wordpress',
+    author='Praiwan N.',
+    author_email='npraiwan@outlook.com',
+    url='https://github.com/author/package_name',
+    download_url='https://github.com/author/repo/tarball/1.0',
+    keywords=['tag1', 'tag2'],
+    classifiers=[],
+)
