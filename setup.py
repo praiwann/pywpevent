0d43b8effd36edc0f0759e1f46d4f8e7d24d02e7
Unknown <npraiwan@outlook.com>
2018-08-06 03:02:38 +0700

    update ver.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pywpevent',
     packages=['pywpevent'],
-    version='0.1.7',
+    version='0.1.8',
     description='Simple event management like wordpress',
     author='Praiwan N.',
     author_email='npraiwan@outlook.com',
